7edec65910aa3d86cbf60d69704c54adf6c77471
marcant94 <mcantelar@gmail.com>
2023-07-07 01:27:59 +0200

    añadido import a base

--- src/App.jsx
+++ src/App.jsx
@@ -64,8 +64,8 @@ const App = props => {
 
                 <div className={estilos.contenedorAplicacion}>
                     <Routes>
-                        <Route exact path={base} element={<Busqueda />} />
-                        <Route exact path={base + "sistema/:id"} element={<Sistema />} />
+                        <Route exact path={base} element={<Busqueda base={base} />} />
+                        <Route exact path={base + "sistema/:id"} element={<Sistema base={base} />} />
                         <Route exact path="*" element={<NoMatch />} />
                     </Routes>
                 </div>
