1b31188111b9815cad6d2a95715800dc26ea1cd0
marcant94 <mcantelar@gmail.com>
2023-07-08 16:53:33 +0200

    nuevo cambio de url

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -19,7 +19,7 @@ const build = async () => {
             return console.log("Error al leer index.html", err);
         }
 
-        var result = data.replaceAll("?v=dev", "?v=" + packageJson.version);
+        var result = data.replaceAll("?v=dev", "?v=" + packageJson.version).replaceAll("./", "/ed-expansion/");
 
         fse.writeFile(carpetaProd + "/index.html", result, "utf8", function(err) {
             if (err) return console.log("Error al escribir index.html", err);
