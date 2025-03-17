1383bd601f780f80bc56586656f16285149698b4
Mario Cantelar <mcantelar@gmail.com>
2025-03-17 19:01:24 +0100

    borramos sistemas, va con api

--- filtrar_sistemas/arrancar.sh
+++
@@ -1,7 +0,0 @@
-
-# En la carpeta debe estar el fichero systemsWithCoordinates.json descargado de https://www.edsm.net/dump/systemsWithCoordinates.json.gz
-
-docker compose up --build
-
-# docker logs --follow edsystem-rust
-
