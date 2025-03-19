c0b8414f0b96deca6b1a94ee546f0701110c503d
Mario Cantelar <mcantelar@gmail.com>
2025-03-19 23:17:50 +0100

    function guardarParametros

--- src/vistas/buscarProducto/BuscarProducto.jsx
+++ src/vistas/buscarProducto/BuscarProducto.jsx
@@ -72,13 +72,12 @@ const BuscarProducto = () => {
         setProducto(nuevoProducto);
     }
 
-    guardarParametros = () => {
+    function guardarParametros() {
         const params = new URLSearchParams(window.location.search);
 
-        // Añadir nuevos parámetros
-        params.set('parametro1', 'nuevoValor1');
-        params.set('parametro2', 'nuevoValor2');
-        params.set('parametro3', 'nuevoValor3');
+        params.set('alcance', alcance);
+        params.set('producto', producto);
+        params.set('idioma', idioma);
 
         // Actualizar la URL sin recargar la página
         const nuevaURL = `${window.location.pathname}?${params.toString()}`;
