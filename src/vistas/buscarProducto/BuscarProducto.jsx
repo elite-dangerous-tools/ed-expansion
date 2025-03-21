f03a570881318abc3641b24507ecdab3382401f3
marcant94 <mcantelar@gmail.com>
2025-03-21 08:25:23 +0100

    añadimos orden

--- src/vistas/buscarProducto/BuscarProducto.jsx
+++ src/vistas/buscarProducto/BuscarProducto.jsx
@@ -21,6 +21,9 @@ const BuscarProducto = () => {
     const [listaProductos, setListaProductos] = useState([]);
     const [producto, setProducto] = useState(parametrosUrl.producto || "0");
     const [idioma, setIdioma] = useState(parametrosUrl.idioma || "es");
+
+    const [orden, setOrden] = useState(parametrosUrl.orden || "distanciasistema");
+
     const [estacionesProducto, setEstacionesProducto] = useState([]);
 
     const idiomasDisponibles = [
@@ -36,24 +39,19 @@ const BuscarProducto = () => {
 
     const alcancesDisponibles = [
         {
-            id: "15",
-            valor: 15,
-            texto: "15 AL",
-        },
-        {
-            id: "30",
-            valor: 30,
-            texto: "30 AL",
-        },
-        {
-            id: "60",
-            valor: 60,
-            texto: "60 AL (Más lento)",
+            id: "50",
+            valor: 50,
+            texto: "50 AL",
         },
         {
             id: "100",
             valor: 100,
-            texto: "100 AL (Muy lento)",
+            texto: "100 AL",
+        },
+        {
+            id: "150",
+            valor: 150,
+            texto: "150 AL (Más lento)",
         },
     ];
 
@@ -93,12 +91,17 @@ const BuscarProducto = () => {
         setProducto(nuevoProducto);
     }
 
+    function cambiaOrden(evento) {
+        setOrden(evento.target.value);
+    }
+
     function guardarParametros() {
         const params = new URLSearchParams(window.location.search);
 
         params.set("alcance", alcance);
         params.set("producto", producto);
         params.set("idioma", idioma);
+        params.set("orden", orden);
 
         // Actualizar la URL sin recargar la página
         const nuevaURL = `${window.location.pathname}?${params.toString()}`;
@@ -146,20 +149,6 @@ const BuscarProducto = () => {
     }
 
     function compararProductos(a, b) {
-        // let valor1 = sentido === "ASC" ? a[orden] : b[orden];
-        // let valor2 = sentido === "ASC" ? b[orden] : a[orden];
-
-        // if (isNaN(valor1) || isNaN(valor2)) {
-        //     // Si alguno no es númerico, ordenamos como texto
-        //     valor1 = valor1.toLowerCase();
-        //     valor2 = valor2.toLowerCase();
-        // } else {
-        //     // Es numérico
-        //     valor1 = parseFloat(valor1);
-        //     valor2 = parseFloat(valor2);
-        // }
-
-        // Por ahora ordenamos en castellano
         let valor1 = "";
         let valor2 = "";
 
@@ -172,6 +161,44 @@ const BuscarProducto = () => {
         } else {
             valor1 = a.id.toLowerCase();
             valor2 = b.id.toLowerCase();
+        }
+
+        if (valor1 < valor2) {
+            return -1;
+        }
+        if (valor1 > valor2) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    function compararEstaciones(a, b) {
+        let valor1 = "";
+        let valor2 = "";
+
+        switch (orden) {
+            case "suministro":
+                // Orden descendente
+                valor1 = parseFloat(b.suministro);
+                valor2 = parseFloat(a.suministro);
+
+                if (valor1 === valor2) {
+                }
+
+                break;
+            case "distanciasistema":
+            default:
+                // Orden ascendente
+                valor1 = parseFloat(a.distanciasistema);
+                valor2 = parseFloat(b.distanciasistema);
+
+                if (valor1 === valor2) {
+                    valor1 = parseFloat(a.distanciaestacion);
+                    valor2 = parseFloat(b.distanciaestacion);
+                }
+
+                break;
         }
 
         if (valor1 < valor2) {
@@ -224,9 +251,11 @@ const BuscarProducto = () => {
     }
 
     function mostrarEstacionesProducto() {
-        // let estacionesConProductosOrdenadas = estacionesProducto.sort(compararEstaciones);
+        let estacionesConProductosOrdenadas = estacionesProducto.sort(compararEstaciones);
 
-        return estacionesProducto.map((fila) => {
+        let ultimaFilaVisualizada = null;
+
+        return estacionesConProductosOrdenadas.map((fila) => {
             const productoFila = listaProductos.find((prod) => prod.id === fila.producto);
 
             let nombreProducto = fila.producto;
@@ -238,18 +267,51 @@ const BuscarProducto = () => {
                 }
             }
 
-            return (
-                <tr key={fila.estacion + "-" + fila.producto}>
-                    <td>{fila.distanciasistema.toFixed(2)} AL</td>
-                    <td>{fila.sistema}</td>
-                    <td>{fila.distanciaestacion} sl</td>
-                    <td>{fila.estacion}</td>
-                    <td>{fila.tipo}</td>
-                    <td>{nombreProducto}</td>
-                    <td>{fila.suministro}</td>
-                    <td>{fila.precio}</td>
-                </tr>
-            );
+            let filaRenderizada = null;
+
+            if (ultimaFilaVisualizada && ultimaFilaVisualizada.sistema === fila.sistema && ultimaFilaVisualizada.estacion === fila.estacion) {
+                filaRenderizada = (
+                    <tr key={fila.estacion + "-" + fila.producto}>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>{nombreProducto}</td>
+                        <td>{fila.suministro}</td>
+                        <td>{fila.precio}</td>
+                    </tr>
+                );
+            } else if (ultimaFilaVisualizada && ultimaFilaVisualizada.sistema === fila.sistema) {
+                filaRenderizada = (
+                    <tr key={fila.estacion + "-" + fila.producto}>
+                        <td></td>
+                        <td></td>
+                        <td>{fila.distanciaestacion} sl</td>
+                        <td>{fila.estacion}</td>
+                        <td>{fila.tipo}</td>
+                        <td>{nombreProducto}</td>
+                        <td>{fila.suministro}</td>
+                        <td>{fila.precio}</td>
+                    </tr>
+                );
+            } else {
+                filaRenderizada = (
+                    <tr key={fila.sistema + "-" + fila.estacion + "-" + fila.producto}>
+                        <td>{fila.distanciasistema.toFixed(2)} AL</td>
+                        <td>{fila.sistema}</td>
+                        <td>{fila.distanciaestacion} sl</td>
+                        <td>{fila.estacion}</td>
+                        <td>{fila.tipo}</td>
+                        <td>{nombreProducto}</td>
+                        <td>{fila.suministro}</td>
+                        <td>{fila.precio}</td>
+                    </tr>
+                );
+            }
+
+            ultimaFilaVisualizada = fila;
+            return filaRenderizada;
         });
     }
 
@@ -283,6 +345,21 @@ const BuscarProducto = () => {
                                 </option>
                             );
                         })}
+                    </select>
+                    &nbsp;&nbsp;
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-sm-12">
+                    <h4>Orden:</h4>
+                </div>
+
+                <div className="col-sm-12 col-md-4">
+                    <label htmlFor="columna">Columna: </label>
+                    <select id="columna" onChange={cambiaOrden} value={orden} disabled={cargando} className={estilos.selectAlcance}>
+                        <option value="distanciasistema">Distancia Sistema</option>
+                        <option value="suministro">Suministro</option>
                     </select>
                     &nbsp;&nbsp;
                 </div>
